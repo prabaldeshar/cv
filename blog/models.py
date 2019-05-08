390dda2e7d095aea3d7fbcc543c858e9862e2bcd
Prabal desahr <prabaldeshar@gmail.com>
2019-05-08 13:36:45 +0545

    Jobs are showing

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
-    pub_date = models.DateField()
-    body = models.CharField(max_length=500)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
