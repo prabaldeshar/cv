7a29dc8cfcb5bcebee569c8309b7b2546f5e1fd7
Prabal desahr <prabaldeshar@gmail.com>
2019-12-24 18:47:54 +0545

    changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfolio-prabal.herokuapp.com']
+ALLOWED_HOSTS = ['portfolio-prabal.herokuapp.com',
+                'localhost']
 
 
 # Application definition
