3762d7c7cf452ac1f4db47e2ca50464ca63d49a2
Prabal desahr <prabaldeshar@gmail.com>
2019-12-24 18:38:38 +0545

    changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
  ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
